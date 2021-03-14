60f6095fecb1186476dfa9cb218262bcb0c7aed2
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-14 13:59:49 -0400

    add class aliases

---
+++ init.php
@@ -0,0 +1,23 @@
+<?php
+
+class_alias(Winter\Forum\Plugin::class, RainLab\Forum\Plugin::class);
+
+class_alias(Winter\Forum\Classes\TopicTracker::class, RainLab\Forum\Classes\TopicTracker::class);
+
+class_alias(Winter\Forum\Components\Channels::class, RainLab\Forum\Components\Channels::class);
+class_alias(Winter\Forum\Components\Member::class, RainLab\Forum\Components\Member::class);
+class_alias(Winter\Forum\Components\Topics::class, RainLab\Forum\Components\Topics::class);
+class_alias(Winter\Forum\Components\Channel::class, RainLab\Forum\Components\Channel::class);
+class_alias(Winter\Forum\Components\Posts::class, RainLab\Forum\Components\Posts::class);
+class_alias(Winter\Forum\Components\Topic::class, RainLab\Forum\Components\Topic::class);
+class_alias(Winter\Forum\Components\RssFeed::class, RainLab\Forum\Components\RssFeed::class);
+class_alias(Winter\Forum\Components\EmbedChannel::class, RainLab\Forum\Components\EmbedChannel::class);
+class_alias(Winter\Forum\Components\EmbedTopic::class, RainLab\Forum\Components\EmbedTopic::class);
+
+class_alias(Winter\Forum\Controllers\Channels::class, RainLab\Forum\Controllers\Channels::class);
+
+class_alias(Winter\Forum\Models\TopicFollow::class, RainLab\Forum\Models\TopicFollow::class);
+class_alias(Winter\Forum\Models\Post::class, RainLab\Forum\Models\Post::class);
+class_alias(Winter\Forum\Models\Member::class, RainLab\Forum\Models\Member::class);
+class_alias(Winter\Forum\Models\Channel::class, RainLab\Forum\Models\Channel::class);
+class_alias(Winter\Forum\Models\Topic::class, RainLab\Forum\Models\Topic::class);
