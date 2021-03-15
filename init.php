00e15524ce0e1cca0bc9f30ff0003f6b01484dd3
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-15 09:55:42 -0400

    prevent creating the class aliases when the original plugin is still installed

--- init.php
+++ init.php
@@ -1,23 +1,25 @@
 <?php
 
-class_alias(Winter\Forum\Plugin::class, Winter\Forum\Plugin::class);
+if (!class_exists(Winter\Forum\Plugin::class)) {
+    class_alias(Winter\Forum\Plugin::class, Winter\Forum\Plugin::class);
 
-class_alias(Winter\Forum\Classes\TopicTracker::class, Winter\Forum\Classes\TopicTracker::class);
+    class_alias(Winter\Forum\Classes\TopicTracker::class, Winter\Forum\Classes\TopicTracker::class);
 
-class_alias(Winter\Forum\Components\Channels::class, Winter\Forum\Components\Channels::class);
-class_alias(Winter\Forum\Components\Member::class, Winter\Forum\Components\Member::class);
-class_alias(Winter\Forum\Components\Topics::class, Winter\Forum\Components\Topics::class);
-class_alias(Winter\Forum\Components\Channel::class, Winter\Forum\Components\Channel::class);
-class_alias(Winter\Forum\Components\Posts::class, Winter\Forum\Components\Posts::class);
-class_alias(Winter\Forum\Components\Topic::class, Winter\Forum\Components\Topic::class);
-class_alias(Winter\Forum\Components\RssFeed::class, Winter\Forum\Components\RssFeed::class);
-class_alias(Winter\Forum\Components\EmbedChannel::class, Winter\Forum\Components\EmbedChannel::class);
-class_alias(Winter\Forum\Components\EmbedTopic::class, Winter\Forum\Components\EmbedTopic::class);
+    class_alias(Winter\Forum\Components\Channels::class, Winter\Forum\Components\Channels::class);
+    class_alias(Winter\Forum\Components\Member::class, Winter\Forum\Components\Member::class);
+    class_alias(Winter\Forum\Components\Topics::class, Winter\Forum\Components\Topics::class);
+    class_alias(Winter\Forum\Components\Channel::class, Winter\Forum\Components\Channel::class);
+    class_alias(Winter\Forum\Components\Posts::class, Winter\Forum\Components\Posts::class);
+    class_alias(Winter\Forum\Components\Topic::class, Winter\Forum\Components\Topic::class);
+    class_alias(Winter\Forum\Components\RssFeed::class, Winter\Forum\Components\RssFeed::class);
+    class_alias(Winter\Forum\Components\EmbedChannel::class, Winter\Forum\Components\EmbedChannel::class);
+    class_alias(Winter\Forum\Components\EmbedTopic::class, Winter\Forum\Components\EmbedTopic::class);
 
-class_alias(Winter\Forum\Controllers\Channels::class, Winter\Forum\Controllers\Channels::class);
+    class_alias(Winter\Forum\Controllers\Channels::class, Winter\Forum\Controllers\Channels::class);
 
-class_alias(Winter\Forum\Models\TopicFollow::class, Winter\Forum\Models\TopicFollow::class);
-class_alias(Winter\Forum\Models\Post::class, Winter\Forum\Models\Post::class);
-class_alias(Winter\Forum\Models\Member::class, Winter\Forum\Models\Member::class);
-class_alias(Winter\Forum\Models\Channel::class, Winter\Forum\Models\Channel::class);
-class_alias(Winter\Forum\Models\Topic::class, Winter\Forum\Models\Topic::class);
+    class_alias(Winter\Forum\Models\TopicFollow::class, Winter\Forum\Models\TopicFollow::class);
+    class_alias(Winter\Forum\Models\Post::class, Winter\Forum\Models\Post::class);
+    class_alias(Winter\Forum\Models\Member::class, Winter\Forum\Models\Member::class);
+    class_alias(Winter\Forum\Models\Channel::class, Winter\Forum\Models\Channel::class);
+    class_alias(Winter\Forum\Models\Topic::class, Winter\Forum\Models\Topic::class);
+}
