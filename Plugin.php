7fb903549796f78b1dce47d2311685e43eea2f18
Marc Jauvin <marc.jauvin@gmail.com>
2021-05-05 22:23:50 -0400

    add registerClassAliases() method

--- Plugin.php
+++ Plugin.php
@@ -140,4 +140,31 @@ class Plugin extends PluginBase
             'winter.forum::mail.member_report' => 'Notification to moderators when a member is reported to be a spammer.'
         ];
     }
+
+    public function registerClassAliases()
+    {
+        /**
+         * To allow compatibility with plugins that extend the original RainLab.Forum plugin,
+         * this will alias those classes to use the new Winter.Forum classes.
+         */
+        return [
+            \Winter\Forum\Plugin::class                   => \RainLab\Forum\Plugin::class,
+            \Winter\Forum\Classes\TopicTracker::class     => \RainLab\Forum\Classes\TopicTracker::class,
+            \Winter\Forum\Components\Channels::class      => \RainLab\Forum\Components\Channels::class,
+            \Winter\Forum\Components\Member::class        => \RainLab\Forum\Components\Member::class,
+            \Winter\Forum\Components\Topics::class        => \RainLab\Forum\Components\Topics::class,
+            \Winter\Forum\Components\Channel::class       => \RainLab\Forum\Components\Channel::class,
+            \Winter\Forum\Components\Posts::class         => \RainLab\Forum\Components\Posts::class,
+            \Winter\Forum\Components\Topic::class         => \RainLab\Forum\Components\Topic::class,
+            \Winter\Forum\Components\RssFeed::class       => \RainLab\Forum\Components\RssFeed::class,
+            \Winter\Forum\Components\EmbedChannel::class  => \RainLab\Forum\Components\EmbedChannel::class,
+            \Winter\Forum\Components\EmbedTopic::class    => \RainLab\Forum\Components\EmbedTopic::class,
+            \Winter\Forum\Controllers\Channels::class     => \RainLab\Forum\Controllers\Channels::class,
+            \Winter\Forum\Models\TopicFollow::class       => \RainLab\Forum\Models\TopicFollow::class,
+            \Winter\Forum\Models\Post::class              => \RainLab\Forum\Models\Post::class,
+            \Winter\Forum\Models\Member::class            => \RainLab\Forum\Models\Member::class,
+            \Winter\Forum\Models\Channel::class           => \RainLab\Forum\Models\Channel::class,
+            \Winter\Forum\Models\Topic::class             => \RainLab\Forum\Models\Topic::class,
+        ];
+    }
 }
